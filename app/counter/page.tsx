ddbea965b707801fd998150d4eff2e30b929d34d
Jay <wer35073@gmail.com>
2026-05-27 16:07:40 +0800

    新增計數器路由

---
+++ app/counter/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function CounterPage() {
+  return (
+    <>
+      <h1>計數器</h1>
+      <hr />
+      <button>0</button>
+    </>
+  );
+}
