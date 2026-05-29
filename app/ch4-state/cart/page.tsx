a8ef020757c7e3c15c27e34d8d28c9b0b7481961
Jay <wer35073@gmail.com>
2026-05-29 15:10:20 +0800

    新增深拷貝

--- app/ch4-state/cart/page.tsx
+++ app/ch4-state/cart/page.tsx
@@ -1,3 +1,5 @@
+"use clint";
+
 import { useState } from 'react';
 
 const initialProducts = [
@@ -21,7 +23,18 @@ const initialProducts = [
 export default function ShoppingCart() {
   const [products, setProducts] = useState(initialProducts);
 
-  function handleIncreaseClick(productId) {}
+  function handleIncreaseClick(productId) {
+    // 1. 從現有的狀態拷貝出一個新的複本
+    // const nextProducts = [...products] //這種寫法不行, 因為深度有兩層(除非他是單純只有一個物件), 這叫淺拷貝(只能拷貝一層), 超過一層以上無法用展開運算子來拷貝
+    // 使用深拷貝語法 :
+    const nextProducts = JSON.parse(JSON.stringify(products)); //深拷貝
+    // 2. 在複本上做修改
+    // 找它是在陣列的哪一個, 找到後把這高物見的count作+1的動作
+    const foundIndex = nextProducts.findIndex((v: any)=> v.id === productId);
+    nextProducts[foundIndex].count++;
+    // 3. 呼叫set方法設定給狀態
+    setProducts(nextProducts)
+  }
 
   return (
     <ul>
