c254c5361efe35fdb43d4cc334b3c9726d687bd9
Jay <wer35073@gmail.com>
2026-06-01 11:11:46 +0800

    todo內容與筆記更新

--- app/ch4-state/todo/page.tsx
+++ app/ch4-state/todo/page.tsx
@@ -1,13 +1,38 @@
 'use client';
 
 import { useState } from 'react';
-
+const initData = [
+  {
+    id: 1,
+    text: '繳電話費',
+  },
+  {
+    id: 2,
+    text: '寫作業',
+  },
+];
 
 export default function TodoPage() {
+  // 記錄待辦事項的狀態
+  const [todos, setTodos] = useState(initData);
+  // 宣告給文字輸入匡使用的狀態
+  const [inputText, setInputText] = useState('aaa');
   return (
     <>
       <h1>待辦事項</h1>
       <hr />
+      <input
+        type="text"
+        value={inputText}
+        onChange={(e) => {
+          setInputText(e.target.value);
+        }}
+      />
+      <ul>
+        {todos.map((todo) => {
+          return <li key={todo.id}>{todo.text}</li>;
+        })}
+      </ul>
     </>
   );
 }
