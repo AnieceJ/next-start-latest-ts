9409a8d72a7f21af1b646fb1d3c69d29043674af
Jay <wer35073@gmail.com>
2026-06-01 15:59:22 +0800

    新增child結構與parent呈現渲染

--- app/ch5-props/pc/_components/child.tsx
+++ app/ch5-props/pc/_components/child.tsx
@@ -1,12 +1,42 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import { ReactNode } from 'react';
 
+interface Data {
+  x?: number;
+  y?: number;
+}
 
-export default function Child() {
+// props
+interface ChildProps {
+  title?: string;
+  price?: number;
+  isConnected?: boolean;
+  aa?: number[];
+  oa?: Data;
+  sum?: (a: number, b: number) => number;
+  jsx?: ReactNode;
+}
+export default function Child({
+  title = '',
+  price = 0,
+  isConnected = true,
+  aa = [],
+  oa = {},
+  sum = (a, b) => a + b,
+  jsx = <></>,
+}: ChildProps) {
   return (
     <>
-      <div>Components Child</div>
+      <div>Child(子女元件)</div>
+      <p>title={title}</p>
+      <p>price={price}</p>
+      {/* 布林值不會呈現渲染 */}
+      <p>isConnected={JSON.stringify(isConnected)}</p>
+      <p>aa={JSON.stringify(aa)}</p>
+      <p>oa={JSON.stringify(oa)}</p>
+      <p>sum(1,2)={sum(1, 2)}</p>
+      {/* JSX語法 */}
     </>
   );
 }
--- app/ch5-props/pc/_components/parent.tsx
+++ app/ch5-props/pc/_components/parent.tsx
@@ -1,12 +1,20 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
-
+import Child from './child';
 
 export default function Parent() {
   return (
     <>
-      <div>Components Parent</div>
+      <h2>Parent(父母元件)</h2>
+      <Child
+        title="目前正在教第5章"
+        price={100}
+        isConnected={false}
+        aa={[1, 2, 3]}
+        oa={{ x: 1, y: 2 }}
+        sum={(a, b) => a + b}
+        jsx={<p>hello</p>}
+      />
     </>
   );
 }
