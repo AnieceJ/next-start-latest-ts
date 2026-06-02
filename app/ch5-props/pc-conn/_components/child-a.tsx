b5aefc1733d54fc4100b2f19b660534039774978
Jay <wer35073@gmail.com>
2026-06-02 09:46:15 +0800

    抓老師git

--- app/ch5-props/pc-conn/_components/child-a.tsx
+++ app/ch5-props/pc-conn/_components/child-a.tsx
@@ -4,26 +4,17 @@
 interface ChildAProps {
   setPData: (s: string) => void;
 }
-<<<<<<< HEAD
-
-export default function ChildA({ setPData }: ChildAProps) {
-  const cData = '子女a內部資料';
-=======
 
 export default function ChildA({ setPData = () => {} }: ChildAProps) {
   const cData = '子女a內部資料';
 
->>>>>>> upstream
   return (
     <>
       <div>ChildA(子女元件A)</div>
       <button
         onClick={() => {
-<<<<<<< HEAD
-=======
           // 子女中得到父母傳來的設定狀態的方法，呼叫後回傳資料回父母元件
           // set狀態的方法因為有副作用，只能在事件處理函式或useEffect鉤子中呼叫
->>>>>>> upstream
           setPData(cData);
         }}
       >
