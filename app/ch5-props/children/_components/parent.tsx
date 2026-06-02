0af323033bb6a8fe85f61bc75de0e39337b4532c
Jay <wer35073@gmail.com>
2026-06-02 11:00:17 +0800

    page.tsx+parent+child合體範例

---
+++ app/ch5-props/children/_components/parent.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { ReactNode } from 'react';
+
+export interface ParentProps {
+  children: ReactNode;
+}
+
+export default function Parent({ children }: ParentProps) {
+  return (
+    <>
+      <div style={{ color: 'red', fontSize: 30, border: '1px sold #ccc' }}
+      >
+        {children}
+      </div>
+    </>
+  );
+}
---
+++ app/ch5-props/children/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Parent from './_components/parent';
+import Child from './_components/child';
+
+export default function ChildrenPage() {
+  return (
+    <>
+      <h1>props.children範例</h1>
+      <hr />
+      <Parent>Hello</Parent>
+      <Parent>
+        <Child />
+      </Parent>
+    </>
+  );
+}
