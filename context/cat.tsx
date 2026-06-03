88a9d9e3f63873b53c2566dfd4f6186a8fa92f86
Jay <wer35073@gmail.com>
2026-06-03 15:50:00 +0800

    抓老師的檔案

---
+++ context/cat.tsx
@@ -0,0 +1,43 @@
+// 第1步 建立context
+'use client'; // context與Provider元件是客戶端元件的應用
+
+import { createContext, useState, useContext } from 'react';
+
+interface CartContextType {
+
+}
+
+// 使用 null 作為預設值，這是 React Context 的常見做法
+const CartContext = createContext<CartContextType | null>(null);
+// 設定context的名稱，這是會在react devtools(瀏覽器擴充)上面會看到，方便除錯用(可選的)
+CartContext.displayName = 'CartContext';
+
+// 第3-1步，建立Provider元件
+export function CartProvider({ children }: { children: React.ReactNode }) {
+  // 宣告狀態
+  const [Cart, setCart] = useState< >();
+
+  // 切換佈景狀態的函式
+  const toggleCart = () => {
+    const nextCart = Cart === 'light' ? 'dark' : 'light';
+    setCart(nextCart);
+  };
+
+  return (
+    <CartContext.Provider value={{ Cart, toggleCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+// 自訂名稱鉤子(先包裝useContext+CartContext)
+export const useCart = () => {
+  // 從context值中解構出value中的值和切換函式
+  const context = useContext(CartContext);
+
+  if (!context) {
+    throw Error('it must be used within CartProvider');
+  }
+
+  return context;
+};
