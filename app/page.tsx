a058c2c6cda64e595da9297e2b861ba9a6a77d6b
Jay <wer35073@gmail.com>
2026-06-03 14:55:52 +0800

    在Homepage做測試切換亮暗主題

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
 'use client'; // 意義是在客戶端使用，代表這個檔案中的元件是`客戶端元件(RCC)`
 // 如果沒有寫'use client'就是 `伺服器元件(RSC)`
 
+import { useTheme } from '@/context/theme';
+
 export default function HomePage() {
+  const { theme } = useTheme();
+
+  const color = theme === 'dark' ? 'white' : 'black';
+  const bgColor = theme === 'dark' ? 'black' : 'white';
+
   return (
     <>
-      <div>HomePage</div>
+      <div
+        style={{
+          height: '100vh',
+          width: '100%',
+          backgroundColor: bgColor,
+          color: color,
+        }}
+      >
+        <h1>HomePage</h1>
+        <p>這裡有一些文字</p>
+      </div>
     </>
   );
 }
